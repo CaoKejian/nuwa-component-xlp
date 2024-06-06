61eeb6864a3685c06ec1d152c4484a350c99078c
caokejian <1849201815@qq.com>
2024-06-07 01:28:16 +0800

    webpack: 完善打包

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.tsx',
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:6].js', // 设置哈希长度为六位
+    path: path.resolve(__dirname, '../dist'), // 输出到 build 的上级目录的 dist
     clean: true
   },
   resolve: {
@@ -19,26 +22,54 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:6].css' // 设置哈希长度为六位
     })
   ],
   optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
+        },
+        parallel: true,
+        extractComments: false, 
+      }),
+      new CssMinimizerPlugin()
+    ],
     splitChunks: {
       chunks: 'all',
     }
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist')
-    },
-    compress: true,
-    port: 9000,
-    hot: true
   }
 };
